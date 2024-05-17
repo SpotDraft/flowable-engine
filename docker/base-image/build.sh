22a6ebb9f05b87e806546abafdb252a96554164b
dheerajmav <dheeraj.mavilla@spotdraft.com>
2024-05-17 14:29:34 +0530

    upgrade base image

--- docker/base-image/build.sh
+++ docker/base-image/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
-readonly IMAGE=${1:-"flowable/flowable-jre:11.0.15"}
+readonly IMAGE=${1:-"flowable-jre:11.0.23"}
 echo "Image name: ${IMAGE}"
 
 echo "Building image..."
-docker build -t ${IMAGE} -f Dockerfile .
+docker build --platform linux/amd64 -t ${IMAGE} -f Dockerfile .
